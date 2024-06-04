0055f6671dd3224f36d744b8744fd60d8ee426d5
Lucas <lucasdeveloper08@gmail.com>
2024-06-04 12:40:28 -0300

    criando comando para mudar canais e volumes

--- SmartTv/src/SmartTv.java
+++ SmartTv/src/SmartTv.java
@@ -1,6 +1,30 @@
 public class SmartTv {
-    boolean ligado = false;
+    boolean ligada = false;
     int canal = 1;
     int volume = 25;
 
+    public void mudarCanal (int NovoCanal){
+        canal = NovoCanal;
+    }
+
+         public void aumentarCanal(){
+        canal++;
+    }
+         public void diminuircanal (){
+        canal--;
+    }
+
+
+    public void ligar(){
+        ligada = true;
+    } 
+    public void desligar(){
+        ligada = false;
+    } 
+    public void AumentarVolume(){
+        volume++;
+    }
+    public void DimimuirVolume(){
+        volume--;
+    }
 }
--- SmartTv/src/Usuario.java
+++ SmartTv/src/Usuario.java
@@ -1,11 +1,29 @@
 public class Usuario {
     public static void main(String[] args) throws Exception {
       
-      SmartTv SmartTv = new SmartTv();
+      SmartTv smartTv = new SmartTv();
+      System.out.println("Canal Atual: "+smartTv.canal);
 
-      System.out.println("A Tv esta ligada? "+SmartTv.ligado);
-    System.out.println("Em que canal a Tv esta ligada? " +SmartTv.canal);
-    System.out.println("Qual o Volume da Tv? "+SmartTv.volume);
+     smartTv.mudarCanal(13);
+     System.out.println("Canal Atual: "+smartTv.canal);
+      
+        smartTv.DimimuirVolume();
+        smartTv.DimimuirVolume();
+        smartTv.DimimuirVolume();
+        smartTv.AumentarVolume();
+      System.out.println("Volume atual: "+smartTv.volume);
+
+        System.out.println("Tv esta Ligada? "+smartTv.ligada);
+        System.out.println("Canal Atual: "+smartTv.canal);
+        System.out.println("Volume atual: "+smartTv.volume);
+
+      smartTv.ligar();
+      System.out.println("Atv esta ligada? " +smartTv.ligada);
+
+      smartTv.desligar();
+
+      System.out.println( "a Tv desligada? " +smartTv.ligada);
+
   }
       
       
