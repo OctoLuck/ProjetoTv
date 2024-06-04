364da2e222d41c0a9c5d3004a4808c70c88bc52c
Lucas <lucasdeveloper08@gmail.com>
2024-06-03 23:44:02 -0300

    Projeto Tv

---
+++ SmartTv/src/Usuario.java
@@ -0,0 +1,13 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+      
+      SmartTv SmartTv = new SmartTv();
+
+      System.out.println("A Tv esta ligada? "+SmartTv.ligado);
+    System.out.println("Em que canal a Tv esta ligada? " +SmartTv.canal);
+    System.out.println("Qual o Volume da Tv? "+SmartTv.volume);
+  }
+      
+      
+
+}
